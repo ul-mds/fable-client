1eda77dc13047a468f2596f76d9517ff4cdae81a
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-06-05 12:00:41 +0200

    Test `new_error_from_response()`

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,4 @@
+import httpx
 import pytest
 from fable_model import (
     BitVectorEntity,
@@ -19,6 +20,8 @@ from fable_model import (
     CLKFilter,
 )
 
+from fable_client import FableError
+from fable_client._client import new_error_from_response, ValidationErrorResponse, GenericErrorResponse
 from tests.helpers import generate_person
 
 pytestmark = pytest.mark.integration
@@ -95,3 +98,33 @@ def test_mask(fable_client, uuid4_factory, faker):
 
     assert len(entities) == len(r.entities)
     assert input_ids == output_ids
+
+
+def test_validation_error():
+    request = httpx.Request("POST", "http://test/match/")
+    response = httpx.Response(
+        httpx.codes.UNPROCESSABLE_ENTITY.value,
+        json={"detail": [{"loc": ["body"], "msg": "field required", "type": "missing"}]},
+        request=request,
+    )
+    error = new_error_from_response(response)
+
+    assert isinstance(error, FableError)
+    assert isinstance(error.error_response, ValidationErrorResponse)
+    assert ": invalid request" in str(error)
+    assert error.error_type == "validation"
+
+
+def test_generic_error():
+    request = httpx.Request("POST", "http://test/match/")
+    response = httpx.Response(
+        httpx.codes.INTERNAL_SERVER_ERROR.value,
+        json={"detail": "fake internal server error"},
+        request=request,
+    )
+    error = new_error_from_response(response)
+
+    assert isinstance(error, FableError)
+    assert isinstance(error.error_response, GenericErrorResponse)
+    assert "fake internal server error" in str(error)
+    assert error.error_type == "default"
